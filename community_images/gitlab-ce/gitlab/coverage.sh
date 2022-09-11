b771cb5269a9edd1350ab678e5d2f83a5b8b3da3
Ankit Kumar <ankit@rapidfort.com>
2022-09-11 19:20:42 +0530

    update

--- community_images/gitlab-ce/gitlab/coverage.sh
+++ community_images/gitlab-ce/gitlab/coverage.sh
@@ -5,32 +5,32 @@ set -x
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-function test_nats() {
-   local NAMESPACE=$1
-   local HELM_RELEASE=$2
+function test_gitlab() {
+   # get the personal access token for gitlab access
+   expires_at=$(date -d '+1 day' +%Y-%m-%d)
+   access_token=$("$SCRIPTPATH"/gen_gitlab_token.py "testtoken" "$expires_at")
 
-   NATS_USER=$(kubectl get secret --namespace "${NAMESPACE}" "${HELM_RELEASE}" -o jsonpath='{.data.*}' | base64 -d | grep -m 1 user | awk '{print $2}' | tr -d '"')
-   NATS_PASS=$(kubectl get secret --namespace "${NAMESPACE}" "${HELM_RELEASE}" -o jsonpath='{.data.*}' | base64 -d | grep -m 1 password | awk '{print $2}' | tr -d '"')
-   echo -e "Client credentials:\n\tUser: $NATS_USER\n\tPassword: $NATS_PASS"
+   # create a new project in the gitlab
+   curl -k --request POST --header "PRIVATE-TOKEN: $access_token" \
+    --header "Content-Type: application/json" \
+    --data '{"name": "new_project", "description": "New Project", "initialize_with_readme": "true", "visibility": "public"}'\
+     --url 'https://localhost:4443/api/v4/projects/'
 
-   # clean up the pod with name nats-release-client first if present already
-   kubectl delete pod nats-release-client --namespace "${NAMESPACE}" --ignore-not-found=true
-   kubectl run nats-release-client --restart='Never' --env="NATS_USER=$NATS_USER" --env="NATS_PASS=$NATS_PASS" --image docker.io/bitnami/golang --namespace "${NAMESPACE}" --command -- sleep infinity
-    # wait for nats client to come up
-   kubectl wait pods nats-release-client -n "${NAMESPACE}" --for=condition=ready --timeout=10m
-   echo "#!/bin/bash
-   GO111MODULE=off go get github.com/nats-io/nats.go
-   cd \"\$GOPATH\"/src/github.com/nats-io/nats.go/examples/nats-pub && go install && cd || exit
-   cd \"\$GOPATH\"/src/github.com/nats-io/nats.go/examples/nats-echo && go install && cd || exit
-   nats-echo -s nats://$NATS_USER:$NATS_PASS@${HELM_RELEASE}.${NAMESPACE}.svc.cluster.local:4222 SomeSubject &
-   nats-pub -s nats://$NATS_USER:$NATS_PASS@${HELM_RELEASE}.${NAMESPACE}.svc.cluster.local:4222 -reply Hi SomeSubject 'Hi everyone'" > "$SCRIPTPATH"/commands.sh
+   # clone the new project and make a test commit
+   git -c http.sslVerify=false clone https://root:adminadmin@localhost:4443/root/new_project.git
 
-   chmod +x "$SCRIPTPATH"/commands.sh
-   POD_NAME="nats-release-client"
-   kubectl -n "${NAMESPACE}" cp "${SCRIPTPATH}"/commands.sh "${POD_NAME}":/tmp/common_commands.sh
+   cd new_project
+   echo "test commit" > test_file.txt
+   git add test_file.txt
+   git commit -m "update"
+   git -c http.sslVerify=false push -u origin main
 
-   kubectl -n "${NAMESPACE}" exec -i "${POD_NAME}" -- bash -c "/tmp/common_commands.sh"
-
-   # delete the generated commands.sh
-   rm "$SCRIPTPATH"/commands.sh
+   # delete and reclone the project
+   cd ..
+   rm -rf new_project
+   git -c http.sslVerify=false clone https://root:adminadmin@localhost:4443/root/new_project.git
+   cd new_project
+   if [ ! -f test_file.txt ]; then
+      exit 1
+   fi
 }
